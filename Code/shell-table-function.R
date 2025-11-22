8234be2cc771513eefe58ed403044d0d53e5dd3d
ShuaiZ2020 <shuai.zhu2001@gmail.com>
2025-11-22 20:28:24 +0800

    commit before fold lbtest

--- Code/shell-table-function.R
+++ Code/shell-table-function.R
@@ -7,22 +7,29 @@ crf_dictionary_sliced <- crf_dictionary_full%>%select(1:4)
 mock_ready_crf <- crf_filed_sliced%>%
   left_join(crf_dictionary_sliced, by = join_by(dataDictionaryOID == dataDictionaryOID))
 
-make_mock_shell <- function(formoid, row_num=3, date_format = "YYYY-MM-DD", 
-                            col_combine = c(),
-                            col_pre = c("受试者随机号", "随机组别"), replace_string = F, 
-                            col_remove = c("单位", "备注"), filter_yes = "",
-                            seed = "123"){
+
+
+make_mock_shell <- function(formoid, row_num=3, 
+                            col_combine = c(), col_pre = c("受试者随机号", "随机组别"), col_remove = c("单位", "备注"),
+                            replace_string = F, filter_yes = "", seed = "123",
+                            ismedDrug = F, iswhodrug = F,
+                            fold_col = 
+                            ){
   set.seed(seed)
+  footnote_vec <- c()
   mock_ready_crf <- mock_ready_crf%>%filter(formOID==formoid)
   fieldnames <- mock_ready_crf$fieldName%>%unique()%>%setdiff(., col_remove)
-
-  create_column<- function(fieldname, row_num, date_format){
-    cont.type = mock_ready_crf%>%filter(fieldName==fieldname)%>%pull(controlType)%>%head(1)
+  create_column<- function(fieldname, row_num, date_format, replace_string = F){
+    mock_ready_crf_filename <- mock_ready_crf%>%filter(fieldName==fieldname)
+    cont.type = mock_ready_crf_filename%>%pull(controlType)%>%head(1)
     if(cont.type=="水平单选框"){
-      res <- mock_ready_crf%>%filter(fieldName==fieldname)%>%pull(itemDataString)%>%sample(row_num, replace = T)
+      res <- mock_ready_crf_filename%>%pull(itemDataString)%>%sample(row_num, replace = T)
     }else if(cont.type=="垂直单选框"){
-      res <- mock_ready_crf%>%filter(fieldName==fieldname)%>%pull(ordinal)%>%sample(row_num, replace = T)%>%
+      res <- mock_ready_crf_filename%>%pull(ordinal)%>%sample(row_num, replace = T)%>%
         as.character()
+      footnote <- paste0(fieldname,"：",
+                         paste0(paste0(mock_ready_crf_filename$ordinal, "=", mock_ready_crf_filename$itemDataString), collapse = "，"))
+      footnote_vec <<- c(footnote_vec, footnote)
     }
     else if(cont.type == "日期框"){
       res <- sample(x = date_format, row_num, replace = T)
@@ -30,16 +37,15 @@ make_mock_shell <- function(formoid, row_num=3, date_format = "YYYY-MM-DD",
       res <- sample(x = "xxx", row_num, replace = T)
     }
     if(replace_string){
-      res <- case_when(str_detect(res, "其他，请")~"其他/xxxx",
-                       .default = res)
-      res <- case_when(str_detect(res, "否，请")~"否/xxxx",
+      res <- case_when(str_detect(res, "，请")~str_replace(res, "，请.*", "/xxx"),
                        .default = res)
     }
-    return(c(res))
+    return(res)
   }
-  field_w_value <- lapply(fieldnames, \(x) create_column(fieldname = x, row_num=row_num, date_format = date_format))
+  field_w_value <- lapply(fieldnames, \(x) create_column(fieldname = x, row_num=row_num, date_format = date_format, replace_string = replace_string))
   setNames(field_w_value, fieldnames)
   shell_body <- as.data.frame(field_w_value, col.names = fieldnames, check.names =F)
+  ### add column are not in form
   if(length(col_pre)==0){
     res_tab <- shell_body
   }else{
@@ -47,6 +53,7 @@ make_mock_shell <- function(formoid, row_num=3, date_format = "YYYY-MM-DD",
     shell_pre <- as.data.frame(field_pre, col.names = col_pre, check.names =F)
     res_tab <- cbind(shell_pre, shell_body)
   }
+  ### combine columns to one
   if(length(col_combine)!=0){
     new_colname <- paste0(col_combine,collapse = "\n/")
     res_tab <- res_tab%>%unite(!!new_colname,
@@ -54,45 +61,49 @@ make_mock_shell <- function(formoid, row_num=3, date_format = "YYYY-MM-DD",
                     sep = "\n/",
                     remove = T)
   }
+  ### if use who drug coding, add ATC/PN column and footnote
+  if(iswhodrug){
+    res_tab <- res_tab%>%add_column("ATC\n/PN"=sample("xxx\n/xxx", row_num, replace = T), .after = 3)
+    footnote_vec <- c(footnote_vec, "ATC2：解剖学、治疗学及化学分类法2级，PN：首选药名","依据&DrugDIR.进行编码")
+  }
+  ### if use med drug coding, add SOC/PT column and footnote
+  if(ismedDrug){
+    res_tab <- res_tab%>%add_column("SOC\n/PT"=sample("xxx\n/xxx", row_num, replace = T), .after = 3)
+    footnote_vec <- c(footnote_vec, "SOC：系统器官分类，PT：首选术语", "依据&MedDRA.进行编码")
+  }
+  ### filter columns with value of yes
   if(filter_yes!=""){
     res_tab <- res_tab%>%filter(!!sym(filter_yes)=="是")%>%
       select(-!!sym(filter_yes))
   }
   res_tab[nrow(res_tab)+2,] <- "......"
-  return(res_tab)
+  res_list <- list(table = res_tab,
+                   footnote = add_footnote_path(footnote_vec))
+  return(res_list)
 }
 
-add_footnote <- function(footnote, ismedDrug = F, iswhodrug = F){
-  if(ismedDrug){
-    footnote <- c(footnote,
-                  "SOC：系统器官分类，PT：首选术语",
-                  "依据&MedDRA.进行编码")
-  }
+
+add_footnote_path <- function(footnote){
   
-  if(iswhodrug){
-    footnote <- c(footnote,
-                  "ATC2：解剖学、治疗学及化学分类法2级，PN：首选药名",
-                  "依据&DrugDIR.进行编码")
-  }
-
+  ### footnote
+  res_footnote <- ""
   if(length(footnote)!=0){
-    res <- paste0(c("::: {custom-style='Footnote'}",  
+    res_footnote <- paste0(c("::: {custom-style='Footnote'}",  
                     paste0(footnote, collapse =  "\n\n"), 
-                    ":::",
-                    "\n<br>\n"), 
+                    ":::"
+                    ), 
                   collapse  = "\n")
-    return(res)
-  }else{
-    return("<br>\n")
+
   }
+  ### saspath
+  res_saspath <-   paste0(c("\n\n<br>\n",
+                            "::: {custom-style='路径'}", 
+                            "&lt;路径&gt;/< XXX.SAS>    draft YYYY-MM-DD HH:MM", 
+                            ":::",
+                            "\n<br>",
+                            "<br>\n"), 
+                          collapse  = "\n")
+  res <- paste0(res_footnote, res_saspath)
+  return(res)
 }
 
-add_saspath <- function(){
-  paste0(c("::: {custom-style='路径'}", 
-           
-           "&lt;路径&gt;/< XXX.SAS>    draft YYYY-MM-DD HH:MM", 
-           ":::",
-           "<br>",
-           "<br>"), 
-         collapse  = "\n")
-}
